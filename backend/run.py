af3bc31218ff3734bc5ba2a8d4838efb340f6e9a
danielgaim-visma <daniel.gaim@visma.com>
2024-08-28 09:31:30 +0200

    update

--- backend/run.py
+++ backend/run.py
@@ -1,8 +1,19 @@
-from flask import Flask
+import os
+from flask import Flask, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../frontend/build')
 
-# Your routes and other Flask configurations go here
+# Your existing routes and logic here
+
+# Serve React App
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
