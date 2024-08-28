21ebf55f4dfdc7a090ae996e6cbb1fddc21be872
danielgaim-visma <daniel.gaim@visma.com>
2024-08-28 08:56:00 +0200

    update

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Add the project root directory to the Python path
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from Backend.run import app
 
 if __name__ == "__main__":
