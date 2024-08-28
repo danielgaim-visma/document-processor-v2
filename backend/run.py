01f42eee6bbc83f42fed58ac5dfad745eccc5baf
danielgaim-visma <daniel.gaim@visma.com>
2024-08-28 14:56:39 +0200

    Cannot find app

--- backend/run.py
+++ backend/run.py
@@ -1,7 +1,7 @@
 from app import create_app
 from app.config import config
 
-app = create_app(config['production'])  # Use production config on Heroku
+app = create_app(config['production'])  # or whichever config you're using
 
 if __name__ == '__main__':
     app.run()
